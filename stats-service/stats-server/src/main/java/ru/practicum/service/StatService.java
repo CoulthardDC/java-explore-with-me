11b0651a0dbed3a39c40cc59a57623d0e540c80d
Евгений Чернецкий <Anidalis1@yandex.ru>
2023-05-01 00:22:12 +0300

    refactor: изменить set на list в statServiceImpl

--- stats-service/stats-server/src/main/java/ru/practicum/service/StatService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/service/StatService.java
@@ -6,10 +6,10 @@ import ru.practicum.dto.StatShortDto;
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
 
 public interface StatService {
     StatDto create(StatDto statDto);
 
-    Set<StatShortDto> getStats(LocalDateTime start, LocalDateTime end, Collection<URI> uris, Boolean unique);
+    List<StatShortDto> getStats(LocalDateTime start, LocalDateTime end, Collection<URI> uris, Boolean unique);
 }
